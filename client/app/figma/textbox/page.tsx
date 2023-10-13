bb0f74dbeab34f1579e048909c700fba929e61dd
Claro Recto <clarorecto54@gmail.com>
2023-10-14 02:53:46 +0800

    Preparation for the preview of atom components

---
+++ client/app/figma/textbox/page.tsx
@@ -0,0 +1,9 @@
+export default function TextboxPreview() {
+    return <div //* VIEWPORT
+        className="figmaViewport">
+        <div //* COLUMN 1
+            className="figmaElement">
+            Main
+        </div>
+    </div>
+}
