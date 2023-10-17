2311680dd21b3ff706b416de66f057df5120744d
Claro Recto <clarorecto54@gmail.com>
2023-10-17 18:30:36 +0800

    Menu Design v1.1 Added backdrop animation on focus

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,32 @@
+import MenuForm from "@/components/menu_form";
+import { classMerge } from "@/components/utils";
+import Image from "next/image";
 export default function Home() {
-  return <div
-    className="h-[100vh] w-[100vw] flex justify-center items-center">
-    This is the landing page
+  return <div //* VIEWPORT
+    // "relative h-[100vh] w-[100vw] flex justify-center items-center"
+    className={classMerge(
+      "relative h-[100vh] w-[100vw]", //? Sizing
+      "flex justify-center items-center", //? Display
+    )}>
+    <Image //* BACKGROUND IMAGE
+      className="absolute object-cover -z-[1]"
+      src="/[Image] Background.png"
+      alt=""
+      fill />
+    <div //* BACKDROP
+      className={classMerge(
+        "h-full w-full", //? Sizing
+        "flex justify-center items-center", //? Display
+        "focus-within:backdrop-blur-md focus-within:backdrop-brightness-75", //? Backdrop
+        "transition-all duration-500"
+      )}>
+      <div //* MENU CONTAINER
+        className={classMerge(
+          "p-[32px] px-[64px]", //? Sizing
+          "bg-white rounded-[32px] panelStyle", //? Background
+        )}>
+        <MenuForm />
+      </div>
+    </div>
   </div>
 }
--- client/components/menu_form.tsx
+++ client/components/menu_form.tsx
@@ -41,13 +41,13 @@ export default function MenuForm() {
             <div //* APP NAME & SERVER STATUS
                 className="flex flex-col">
                 <label //* APP NAME
-                    className="text-[16px] font-[600]">
+                    className="text-[16px] font-[600] Unselectable">
                     ScreenSync
                 </label>
                 <div //* SERVER STATUS
                     className="flex gap-[8px] items-center">
                     <label //* LABEL
-                        className="text-[12px] italic">
+                        className="text-[12px] italic Unselectable">
                         {socket.isConnected ? "Connected" : "Disconnected"}
                     </label>
                     <div //* STATUS ICON
@@ -59,30 +59,33 @@ export default function MenuForm() {
             </div>
         </div>
         <div //* DESCRIPTION
-            className="w-full flex flex-col gap-[8px]">
+            className="w-full flex flex-col gap-[8px] max-w-min">
             <label //* HEADER 1
-                className="font-[500] font-[Montserrat] text-[16px]">
+                className="font-[500] font-[Montserrat] text-[16px] min-w-max Unselectable">
                 TUPC Screen Mirroring Solution
             </label>
             <label //* DESCRIPTION
-                className="font-[400] font-[Montserrat] text-[12px]">
+                className="font-[400] font-[Montserrat] text-[12px] Unselectable">
                 Enjoy the power of visual communication and take your educational experience to the next level
             </label>
         </div>
         <form //* FORM
-            className="h-full w-full px-[16px] flex flex-col gap-[16px]">
+            className="h-full w-full px-[16px] flex flex-col gap-[16px]"
+            onSubmit={(thisElement) => { //? What happens on submit
+                thisElement.preventDefault()
+            }}>
             <div //* INPUTS
                 className="flex flex-col gap-[8px]">
                 <Textbox //* USERNAME
                     useIcon iconSrc="/[Icons] Participants.png"
-                    textSize={"small"}
+                    textSize={"small"} maxLength={32} value={inputs.username}
                     placeholder="What is your name?"
                     onChange={(thisElement) => {
                         setInputs({ ...inputs, username: thisElement.target.value })
                     }} />
                 {inputs.username.length > 3 && <Textbox //* MEETING PASSCODE
                     useIcon iconSrc="/[Icons] Key.png"
-                    textSize={"small"}
+                    textSize={"small"} maxLength={32} value={inputs.meetingKey}
                     placeholder="Session key here"
                     onChange={(thisElement) => {
                         setInputs({ ...inputs, meetingKey: thisElement.target.value })
@@ -90,7 +93,7 @@ export default function MenuForm() {
             </div>
             {inputs.username.length > 3 && <Button //* START MEETING
                 useIcon iconSrc="/[Icon] Join.png" iconOverlay
-                textSize={"small"}
+                textSize={"small"} type="submit"
                 className="font-[600]">
                 Start Meeting
             </Button>}
